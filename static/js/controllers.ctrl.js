de473324e58fc8a641c847f05a645d14b455470f
Ryan Bird <dawgillac11@yahoo.com>
2016-11-29 14:45:20 -0800

    late_nov push

--- static/js/controllers.ctrl.js
+++ static/js/controllers.ctrl.js
@@ -44,7 +44,7 @@ app.controller('HomeController', function($interval, $scope) {
 	console.log("HomeController Loaded")
 
 		var dimages= [];
-		var numImages=11;
+		var numImages=4;
 
 		for (i=0; i<numImages; i++) {
 		  // dimages[i]=new Image();
@@ -61,7 +61,7 @@ app.controller('HomeController', function($interval, $scope) {
 				k = 0;
 			}
 			this.image = dimages[k];
-		}.bind(this), 8000);
+		}.bind(this), 3000);
 
 	$scope.$on('$destroy', function() {
 		$interval.cancel(intervalId);
